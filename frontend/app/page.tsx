d7f9e275c14cb4f816c5a975e97f5ea0c67824d6
mawadumich <maikla@umich.edu>
2026-03-09 00:39:52 -0400

    WIP: changes to layout.tsx, seo and remove a few files

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,12 +1,11 @@
 import Link from "next/link";
 // import Image from "next/image";
-// import About from "./about/page";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-    <Link href={{ pathname: "/about", query: { name: "John", age: 25, byeName: "Mike", byeAge: 30}}}>
-      Click to go to about
+    <Link href={{ pathname: "/test", query: { name: "John", age: 25, byeName: "Mike", byeAge: 30}}}>
+      Click to go to test
     </Link>      
     </div>
   );

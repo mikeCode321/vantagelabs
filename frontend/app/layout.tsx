de9a3b812ec12018dc6e7994ae3b928c9868cdf5
mawadumich <maikla@umich.edu>
2026-03-09 01:39:07 -0400

    WIP: backend restructuring

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -52,21 +53,24 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
+
+
   return (
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
-      <head>
+      {/* <head>
         <link rel="icon" href="/favicon.ico" />
-      </head>
+      </head> */}
       <body className="bg-zinc-50 dark:bg-black font-sans">
         <header className="p-4 shadow-md bg-white dark:bg-zinc-900">
           My Navbar
         </header>
 
         <main className="min-h-screen p-6">{children}</main>
-
+        
         <footer className="p-4 text-center text-sm text-zinc-500 dark:text-zinc-400">
           © {new Date().getFullYear()} Vantage Labs. All rights reserved.
         </footer>
+
       </body>
     </html>
   );

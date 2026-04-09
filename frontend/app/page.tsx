10c63e061a6e40a0b2aa8e78d8c5093e48a0b059
Nabeel Hashmi <nabeelhashmi2018@gmail.com>
2026-04-09 00:25:03 -0400

    Added pricingsection to landing page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,12 +1,13 @@
 import Navbar from "@/components/Landing/Navbar";
 import HeroSection  from "@/components/Landing/HeroSection";
-
+import PricingSection from "@/components/Landing/PricingSection";
 
 export default function HomePage() {
   return (
     <main className="min-h-screen bg-black text-white">
       <Navbar />
       <HeroSection />
+      <PricingSection />
     </main>
   );
 }

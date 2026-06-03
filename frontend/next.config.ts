24ae0d02ebc5e57ff3acd61d976cd5fba656653c
Mike Awad <maiklawad311@gmail.com>
2026-06-03 16:10:52 -0400

    serve pages and routes fix

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,11 +1,17 @@
 import type { NextConfig } from "next";
 
+// next.config.ts
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
-  /* config options here */
   output: "export",
+  basePath: isProd ? "/vantagelabs" : "",
+  assetPrefix: isProd ? "/vantagelabs/" : "",
+  trailingSlash: true,
+  images: { unoptimized: true },
   typescript: {
     ignoreBuildErrors: true,
-  }
+  },
 };
 
 export default nextConfig;

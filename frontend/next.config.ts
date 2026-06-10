e5852f298a11d263ae54a6a839804ad2640d2eb9
Mike Awad <maiklawad311@gmail.com>
2026-06-09 20:21:26 -0400

    remove middleware from next config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,5 +1,4 @@
 import type { NextConfig } from "next";
-import { NextResponse } from 'next/server'
 
 const nextConfig: NextConfig = {
   images: { unoptimized: true },
@@ -7,10 +6,5 @@ const nextConfig: NextConfig = {
     ignoreBuildErrors: true,
   },
 };
-
-
-export function middleware() {
-  return new NextResponse('Temporarily offline', { status: 503 })
-}
 
 export default nextConfig;

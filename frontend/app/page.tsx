496863c5b7a7de1a2a9f2db3ab55824deed69da6
Nabeel Hashmi <nabeelhashmi2018@gmail.com>
2026-06-16 01:02:10 -0400

    added footer and fire

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,11 +1,15 @@
 import TopBar from "./(landing)/components/TopBar";
 import LandingDashboardShowcase from "./(landing)/components/DashboardShowcase";
+import Fire from "./(landing)/components/Fire"
+import Footer from "./(landing)/components/Footer"
 export default function Home() {
 
   return (
     <>
       <TopBar/>
       <LandingDashboardShowcase/>
+      <Fire/>
+      <Footer/>
     </>
   );
 }

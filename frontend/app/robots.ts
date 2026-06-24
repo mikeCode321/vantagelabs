5d1c879f683ce6babf7efe1348ac8cfea5c670a9
Mike Awad <maiklawad311@gmail.com>
2026-06-23 23:09:18 -0400

    robots update

--- frontend/app/robots.ts
+++ frontend/app/robots.ts
@@ -5,7 +5,6 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
-      disallow: ["/dashboard", "/settings"], // lock these down once auth is added
     },
     sitemap: "https://firephin.com/sitemap.xml",
   };
